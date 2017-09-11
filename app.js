f610f7bc13c71b2dbe396819d34bbad67042564a
cheeyim <cheeyim@gmail.com>
2017-09-12 00:23:51 +0800

    convert to callback function

--- app.js
+++ app.js
@@ -9,21 +9,29 @@ app.use(bodyparser.urlencoded({
 }))
 app.use(bodyparser.json());
 
+function brainWallet(uinput, callback) {
+    var input = new Buffer(uinput);
+    var hash = bitcore.crypto.Hash.sha256(input);
+    //compress key 
+    //var bn = bitcore.crypto.BN.fromBuffer(hash);
+    var pk = new bitcore.PrivateKey(hash).toWIF();
+    var addy = new bitcore.PrivateKey(hash).toAddress();
+    callback(pk, addy);
+};
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/wallet", function(req, res) {
     var brainsrc = req.body.brainsrc;
-    var input = new Buffer(brainsrc);
-    var hash = bitcore.crypto.Hash.sha256(input);
-    //compress key 
-    //var bn = bitcore.crypto.BN.fromBuffer(hash);
-    var pk = new bitcore.PrivateKey(hash).toWIF();
-    var addy = new bitcore.PrivateKey(hash).toAddress();
-    res.send("The brain wallet of " + brainsrc + "<br />Addy: "
-    + addy + "<br />Private key: " + pk);
     console.log(brainsrc);
+
+    brainWallet(brainsrc, function(priv, addr){
+        res.send("The brain wallet of " + brainsrc + "<br />Addy: "
+        + addr + "<br />Private key: " + priv);
+    });
+    
 });
 
 app.listen(80, function(){
